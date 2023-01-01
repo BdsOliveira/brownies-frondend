7725e63da325ed013381a0e2af35fa5b5d371ed6
Bruno Oliveira <bds.commus@gmail.com>
2023-01-01 10:42:54 -0300

    feat: add general layout

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Layout Header</h1>
+    <Nuxt />
+    <h1>Layout Footer</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {};
+  },
+  methods(){}
+}
+</script>
