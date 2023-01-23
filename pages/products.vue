def4db5d02b89ece39bd3f24fb53c677f013e4b4
Bruno Oliveira <bds.commus@gmail.com>
2023-01-23 01:23:13 -0300

    feat: add products route

---
+++ pages/products.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-wrap p-16">
+    <h1>Products Layout</h1>
+    <NuxtChild />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductsLayout',
+  components: {},
+}
+</script>
---
+++ pages/products/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Products Index Page works</h1>
+    <div class="" v-for="company in companies" :key="company.id">
+      <h1>{{ company.name }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'CompanyPage',
+  components: {},
+  data(){
+    const companies = fetch('http://localhost:8000/api/companies')
+      .then(function(response) {
+        return response.blob();
+      })
+    return {
+      companies
+    }
+  },
+})
+</script>
