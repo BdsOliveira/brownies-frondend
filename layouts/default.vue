dc84efde7a31df4abb1cbc3182c6668e52d394bd
Bruno Oliveira <bds.commus@gmail.com>
2023-01-23 00:25:36 -0300

    feat: create sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,37 @@
 <template>
-  <div>
-    <h1>Layout Header</h1>
-    <Nuxt />
-    <h1>Layout Footer</h1>
+  <div class="container flex flex-row">
+    <SidebarComponent />
+    <div class="m-8">
+      <HeaderComponent />
+      <Nuxt />
+      <FooterComponent />
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: '',
-  data () {
-    return {};
+  name: 'DefaultLayout',
+  components: {
+},
+  data() {
+    return {
+    }
   },
-  methods(){}
 }
 </script>
+
+<style scoped>
+:root{
+  --primary: #4ade80;
+  --primary-alt: #22c55e;
+  --grey: #64748b;
+  --dark: #1e293b;
+  --dark-alt: #334155;
+  --light: #f1f5f9;
+  --sidebar-width: 300px
+}
+
+</style>
+
--- pages/companies/index.vue
+++ pages/companies/index.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
     <h1>Companies Index Page works</h1>
+    <div class="" v-for="company in companies" :key="company.id">
+      <h1>{{ company.name }}</h1>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import { defineComponent } from "vue";
+
+export default defineComponent({
   name: 'CompanyPage',
   components: {},
-}
+  data(){
+    const companies = fetch('http://localhost:8000/api/companies')
+      .then(function(response) {
+        return response.blob();
+      })
+    return {
+      companies
+    }
+  },
+})
 </script>
