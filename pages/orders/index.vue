a52364fe3e322b9a35cc6dc9fbf56dfabe89c468
Bruno Oliveira <bds.commus@gmail.com>
2023-01-27 13:38:53 -0300

    feat: get data from api

--- pages/orders/index.vue
+++ pages/orders/index.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
     <h1>Orders Index Page works</h1>
+    {{ orders }}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ReportPage',
+  name: 'OrdersPage',
   components: {},
+  async asyncData({$axios}){
+    const orders = await $axios.$get('orders');
+    return {
+      orders,
+    }
+  },
+  data(){
+    return{}
+  }
 }
 </script>
--- pages/sellers/index.vue
+++ pages/sellers/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Sellers Index Page works</h1>
+    {{ sellers }}
   </div>
 </template>
 
@@ -8,5 +9,15 @@
 export default {
   name: 'SellerPage',
   components: {},
+  async asyncData({$axios}) {
+    const sellers = await $axios.$get('sellers');
+    return {
+      sellers
+    };
+  },
+  data(){
+    return {
+    }
+  }
 }
 </script>
